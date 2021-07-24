a05cfa16ca9c87706ac5391f869ebffa16a60b80
Sphinxtar <sphinxtar@gmail.com>
2021-07-24 15:59:18 -0700

    draw a square on a blue background 	modified:   app/src/main/java/com/gamename/GameNameView.java

--- app/src/main/java/com/gamename/GameNameView.java
+++ app/src/main/java/com/gamename/GameNameView.java
@@ -3,15 +3,17 @@ package com.gamename;
 import android.content.Context;
 import android.view.SurfaceView;
 import android.view.SurfaceHolder;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.graphics.Color;
+import android.graphics.Paint;
 
 //import android.view.View;
 //import android.content.res.Resources;
 //import android.graphics.Bitmap;
 //import android.graphics.BitmapFactory;
-//import android.graphics.Canvas;
 //import android.graphics.Matrix;
 //import android.view.MotionEvent;
-
 //import java.util.ArrayList;
 //import java.util.List;
 //import java.util.Random;
@@ -50,6 +52,19 @@ public class GameNameView extends SurfaceView implements SurfaceHolder.Callback 
 
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        Paint paint = new Paint();
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setColor(Color.WHITE);
+        Rect r = new Rect(30, 30, 320, 240);
+        super.draw(canvas);
+        if(canvas != null) {
+            canvas.drawRGB(0, 100, 205);
+            canvas.drawRect(r, paint);
+        }
     }
 
     public void update() {
