2964f583e9af63ddab5498673ba7272e64559544
“Sphinxtar” <sphinxtar@gmail.com>
2021-07-25 18:39:05 -0700

    works awesome 	modified:   changeGameName.sh

--- changeGameName.sh
+++ changeGameName.sh
@@ -4,6 +4,7 @@ if [ -z "$1" ]
     echo "Supply a new GameName to use as argument"
     exit 1
 fi
+# look out below
 cd ..
 if [[ ! -d GameName ]];then
     echo "Top directory, 'GameName', does not exist, abandon ship!"
@@ -16,13 +17,13 @@ echo "and com.$low for package"
 echo "removing all git info, you'll need to re-initialize"
 rm -rf GameName/.git 
 rm -f GameName/README.md 
-UpList=`grep -lr GameName GameName | tr '\n' ' '`
+UpList=`grep -lr GameName GameName | grep -v changeGameName.sh | tr '\n' ' '`
 sed -i s/GameName/"$1"/g $UpList
-LowList=`grep -lr gamename GameName | tr '\n' ' '`
+LowList=`grep -lr gamename GameName | grep -v changeGameName.sh | tr '\n' ' '`
 sed -i s/gamename/"$low"/g $LowList
-mv GameName/app/src/main/java/com/gamename/GameNameThread.java GameName/app/src/main/java/com/${1}Thread.java
-mv GameName/app/src/main/java/com/gamename/GameNameView.java GameName/app/src/main/java/com/${1}View.java
 mv GameName/app/src/main/java/com/gamename GameName/app/src/main/java/com/${low}
+mv GameName/app/src/main/java/com/${low}/GameNameThread.java GameName/app/src/main/java/com/${low}/${1}Thread.java
+mv GameName/app/src/main/java/com/${low}/GameNameView.java GameName/app/src/main/java/com/${low}/${1}View.java
 mv GameName/app/src/test/java/com/gamename GameName/app/src/test/java/com/${low}
 mv GameName/app/src/androidTest/java/com/gamename GameName/app/src/androidTest/java/com/${low}
 mv GameName ${1}
