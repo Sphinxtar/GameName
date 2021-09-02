19cf6b8021ee52589c5a21946c9c81e21afe4cfa
Sphinxtar <sphinxtar@gmail.com>
2021-09-01 19:53:19 -0700

    modified:   app/src/main/java/com/gamename/MainActivity.java 	new file:   app/src/main/java/com/gamename/SoundManager.java 	new file:   app/src/main/res/raw/honk.ogg

--- app/src/main/java/com/gamename/MainActivity.java
+++ app/src/main/java/com/gamename/MainActivity.java
@@ -5,10 +5,13 @@ import android.os.Bundle;
 import android.view.Window;
 
 public class MainActivity extends Activity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(new GameNameView(this));
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	SoundManager.getInstance();
+	SoundManager.initSounds(this);
+	SoundManager.loadSounds();
+	requestWindowFeature(Window.FEATURE_NO_TITLE);
+	setContentView(new GameNameView(this));
     }
 }
