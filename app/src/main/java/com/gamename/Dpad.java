c9ef03d9d40c0edb54ad7092d27d76dadf4d55af
Sphinxtar <sphinxtar@gmail.com>
2024-01-29 03:23:28 -0700

    AB-XY

--- app/src/main/java/com/gamename/Dpad.java
+++ app/src/main/java/com/gamename/Dpad.java
@@ -7,7 +7,7 @@ import android.graphics.Rect;
 import android.view.MotionEvent;
 
 public class Dpad {
-    public dpadbutts[] dpadbutt = new dpadbutts[9];
+    public dpadbutts[] dpadbutt = new dpadbutts[13];
     final private Paint paint = new Paint();
     public Dpad(Rect vport) {
 
@@ -16,7 +16,7 @@ public class Dpad {
         dpadbutt[0] = new dpadbutts();
         dpadbutt[0].bounds = new Rect();
         dpadbutt[0].setRection(1);
-        dpadbutt[0].setBounds(vport.right + bsize, vport.top + bsize, vport.right + (bsize * 2), vport.top + (bsize * 2));
+        dpadbutt[0].setBounds(vport.left - (bsize * 4), vport.top + bsize, vport.left - (bsize * 3), vport.top + (bsize * 2));
 
         dpadbutt[1] = new dpadbutts();
         dpadbutt[1].bounds = new Rect();
@@ -59,6 +59,26 @@ public class Dpad {
         dpadbutt[8].bounds = new Rect();
         dpadbutt[8].setRection(9);
         dpadbutt[8].setBounds(dpadbutt[2].getLeft(), dpadbutt[6].getTop(), dpadbutt[2].getRight(), dpadbutt[6].getBottom());
+    // abxy
+        dpadbutt[9] = new dpadbutts();
+        dpadbutt[9].bounds = new Rect();
+        dpadbutt[9].setRection(10); // '0xA'
+        dpadbutt[9].setBounds(vport.right + (bsize * 2), vport.top + bsize, vport.right + (bsize * 3), vport.top + (bsize * 2));
+
+        dpadbutt[10] = new dpadbutts();
+        dpadbutt[10].bounds = new Rect();
+        dpadbutt[10].setRection(11); // '0xA'
+        dpadbutt[10].setBounds(dpadbutt[9].getLeft() - bsize, dpadbutt[9].getTop() + bsize, dpadbutt[9].getLeft(), dpadbutt[9].getBottom() + bsize);
+
+        dpadbutt[11] = new dpadbutts();
+        dpadbutt[11].bounds = new Rect();
+        dpadbutt[11].setRection(11);
+        dpadbutt[11].setBounds(dpadbutt[9].getRight(), dpadbutt[9].getBottom(), dpadbutt[9].getRight() + bsize, dpadbutt[9].getBottom() + bsize);
+
+        dpadbutt[12] = new dpadbutts();
+        dpadbutt[12].bounds = new Rect();
+        dpadbutt[12].setRection(12);
+        dpadbutt[12].setBounds(dpadbutt[10].getRight(), dpadbutt[10].getBottom(), dpadbutt[11].getLeft(), dpadbutt[10].getBottom() + bsize);
     }
 
     public int hitButton(MotionEvent event) {
@@ -74,6 +94,9 @@ public class Dpad {
     public void draw(Canvas canvas){
         int width = 4;
         int shadowidth = 8;
+        float cx;
+        float cy;
+        float radius;
         if(canvas != null) {
             paint.setStyle(Paint.Style.STROKE);
             paint.setColor(Color.WHITE);
@@ -95,6 +118,34 @@ public class Dpad {
             canvas.drawLine(dpadbutt[5].bounds.left + width, dpadbutt[5].bounds.bottom + shadowidth, dpadbutt[5].bounds.right - width, dpadbutt[5].bounds.bottom + shadowidth, paint);
             canvas.drawLine(dpadbutt[7].bounds.left - shadowidth, dpadbutt[7].bounds.top + width, dpadbutt[7].bounds.left - shadowidth, dpadbutt[7].bounds.bottom + shadowidth, paint);
             canvas.drawLine(dpadbutt[7].bounds.left - shadowidth, dpadbutt[7].bounds.bottom + shadowidth, dpadbutt[7].bounds.right - width, dpadbutt[7].bounds.bottom + shadowidth, paint);
+
+            paint.setColor(Color.BLUE);
+            // canvas.drawRect(dpadbutt[9].getBounds(), paint);
+            cx = dpadbutt[9].getBounds().exactCenterX();
+            cy = dpadbutt[9].getBounds().exactCenterY();
+            radius = (float)dpadbutt[9].getBounds().width() / 2;
+            canvas.drawCircle(cx, cy, radius, paint);
+
+            paint.setColor(Color.GREEN);
+            // canvas.drawRect(dpadbutt[10].getBounds(), paint);
+            cx = dpadbutt[10].getBounds().exactCenterX();
+            cy = dpadbutt[10].getBounds().exactCenterY();
+            radius = (float)dpadbutt[10].getBounds().width() / 2;
+            canvas.drawCircle(cx, cy, radius, paint);
+
+            paint.setColor(Color.RED);
+            // canvas.drawRect(dpadbutt[11].getBounds(), paint);
+            cx = dpadbutt[11].getBounds().exactCenterX();
+            cy = dpadbutt[11].getBounds().exactCenterY();
+            radius = (float)dpadbutt[11].getBounds().width() / 2;
+            canvas.drawCircle(cx, cy, radius, paint);
+
+            paint.setColor(Color.YELLOW);
+            // canvas.drawRect(dpadbutt[12].getBounds(), paint);
+            cx = dpadbutt[12].getBounds().exactCenterX();
+            cy = dpadbutt[12].getBounds().exactCenterY();
+            radius = (float)dpadbutt[12].getBounds().width() / 2;
+            canvas.drawCircle(cx, cy, radius, paint);
         }
     }
 
